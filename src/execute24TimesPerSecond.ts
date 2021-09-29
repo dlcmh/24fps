3426163301a7ae2f1d7ab69ebb3384eaab7a5807
David Chin <dlcmhd@me.com>
2021-09-29 13:27:11 +0800

    feature

--- src/execute24TimesPerSecond.ts
+++ src/execute24TimesPerSecond.ts
@@ -1,3 +1,10 @@
-export function execute24TimesPerSecond() {
-  console.log('lol')
+// https://nodejs.org/en/docs/guides/timers-in-node/
+
+const ONE_SECOND = 1000
+const INTERVAL = ONE_SECOND / 24
+
+export async function execute24TimesPerSecond(fn: () => void | Promise<void>) {
+  const interval = setInterval(fn, INTERVAL)
+
+  setTimeout(() => clearInterval(interval), Number(interval) * 5, interval)
 }
--- test/execute24TimesPerSecond.test.ts
+++ test/execute24TimesPerSecond.test.ts
@@ -1,5 +1,17 @@
 import { execute24TimesPerSecond } from 'src/execute24TimesPerSecond'
+import { appendFileSync, writeFileSync } from 'fs'
 
-test('prints lol 24 times per second', () => {
-  execute24TimesPerSecond()
+test('appends to file', async () => {
+  const file = 'test/scratch/sample.txt'
+  const start = new Date().getTime()
+
+  writeFileSync(file, String(`start: ${start}\n`), 'utf8')
+
+  await execute24TimesPerSecond(() => {
+    const end = new Date().getTime()
+    const duration = end - start
+
+    appendFileSync(file, String(`end:   ${end}\n`), 'utf8')
+    appendFileSync(file, String(`duration: ${duration}\n`), 'utf8')
+  })
 })
